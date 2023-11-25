9763b19dd1e56fb618c094a2baaf53b3e0768c21
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2023-11-25 22:27:13 +0100

    Fixed AbstractForm

--- src/Module.php
+++ src/Module.php
@@ -1,14 +1,14 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace VitesseCms\Form;
 
 use Phalcon\Di\DiInterface;
-use VitesseCms\Block\Repositories\BlockFormBuilderRepository;
-use VitesseCms\Block\Repositories\BlockRepository;
 use VitesseCms\Communication\Repositories\NewsletterRepository;
 use VitesseCms\Core\AbstractModule;
 use VitesseCms\Datagroup\Repositories\DatagroupRepository;
 use VitesseCms\Form\Repositories\RepositoryCollection;
+use VitesseCms\Language\Models\LanguageIterator;
 use VitesseCms\Language\Repositories\LanguageRepository;
 
 class Module extends AbstractModule
@@ -16,10 +16,13 @@ class Module extends AbstractModule
     public function registerServices(DiInterface $di, string $string = null)
     {
         parent::registerServices($di, 'Form');
-        $di->setShared('repositories', new RepositoryCollection(
-            new LanguageRepository(),
-            new DatagroupRepository(),
-            new NewsletterRepository()
-        ));
+        $di->setShared(
+            'repositories',
+            new RepositoryCollection(
+                new LanguageRepository(LanguageIterator::class),
+                new DatagroupRepository(),
+                new NewsletterRepository()
+            )
+        );
     }
 }
